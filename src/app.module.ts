cbf1b5670ac43df9e2c9ce6025e8aa43e8aaa6d1
Dlpzack <danny.lopez@acklenavenue.com>
2025-05-15 09:08:53 -0600

    feat: add DeelModule to AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import DeelModule from './integrations/deel/deel.module';
 
 @Module({
-  imports: [],
+  imports: [DeelModule],
   controllers: [AppController],
   providers: [AppService],
 })
